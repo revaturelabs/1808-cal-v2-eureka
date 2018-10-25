9a1b99afbbbd3d4226cefc46a78aac6ea89ac2c2
aricrhodes <42727692+aricrhodes@users.noreply.github.com>
2018-10-25 11:03:59 -0400

    Eureka feature (#1)
    * Create .gitignore
    
    * Eureka service

---
+++ CaliberEureka/src/main/java/com/revature/caliber/eureka/CaliberEurekaApplication.java
@@ -0,0 +1,12 @@
+package com.revature.caliber.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaliberEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaliberEurekaApplication.class, args);
+	}
+}
