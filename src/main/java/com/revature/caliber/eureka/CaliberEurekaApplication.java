c3ea7a75f323ac38cd8ce67e0fd887c75105c49d
Quintin Donnelly <quinndonnelly22@gmail.com>
2019-05-02 14:49:09 -0400

    Testing AWS enabled config

--- src/main/java/com/revature/caliber/eureka/CaliberEurekaApplication.java
+++ src/main/java/com/revature/caliber/eureka/CaliberEurekaApplication.java
@@ -2,7 +2,12 @@ package com.revature.caliber.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.commons.util.InetUtils;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+
+import com.netflix.appinfo.AmazonInfo;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -10,5 +15,13 @@ public class CaliberEurekaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CaliberEurekaApplication.class, args);
+	}
+	
+	@Bean
+	public EurekaInstanceConfigBean eurekaInstanceConfig(InetUtils inetUtils) {
+	  EurekaInstanceConfigBean b = new EurekaInstanceConfigBean(inetUtils);
+	  AmazonInfo info = AmazonInfo.Builder.newBuilder().autoBuild("eureka");
+	  b.setDataCenterInfo(info);
+	  return b;
 	}
 }
